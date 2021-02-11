b859edc9490ce2d2d85d98fd23d8f645ca8eaf5e
Lucrèce des Courtis <ldes-cou@student.42.fr>
2021-02-11 23:46:00 +0100

    erreur de retour de la fonction

--- fT_parser.c
+++ fT_parser.c
@@ -6,7 +6,7 @@
 /*   By: ldes-cou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 14:55:41 by ldes-cou          #+#    #+#             */
-/*   Updated: 2021/02/11 15:37:55 by ldes-cou         ###   ########.fr       */
+/*   Updated: 2021/02/11 22:35:34 by Sophie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,9 @@ void ft_tab(const char *str, va_list *ap, t_data *data)
 	{
 		if (str[data->i] == '%')
 		{
-			data->i++;
-			if (str[data->i] == 'c')
+			if (str[data->i + 1] == 'c')
 				data->fun_array[0](ap);
-			if (str[data->i] == 's')
+			if (str[data->i + 1] == 's')
 				data->fun_array[1](ap);
 			if (str[data->i] == 'p')
 				data->fun_array[2](ap);
@@ -37,10 +36,14 @@ void ft_tab(const char *str, va_list *ap, t_data *data)
 			if (str[data->i] == 'x')
 				data->fun_array[7](ap);
 			if (str[data->i] == 'X')
-				data->fun_array[8](ap);	
+				data->fun_array[8](ap);
+			data->i += 2;
 		}
 		else
+		{
 			ft_putchar(str[data->i], data);
-		data->i++;
+			data->i++;
+		}
+			
 	}
 }	
